fb6d8e92618fece5e3595fa4891da674d3d2dced
gabriel <gmaher2@hotmail.com>
2015-05-12 20:28:44 -0700

    test

--- src/EMDUtil.jl
+++ src/EMDUtil.jl
@@ -1,6 +1,7 @@
 #Function to find local maximums and minimums of a function
 # assuming the function is smooth, we just simply check
 # y(i-1) < y(i) > y(i+1) for max and y(i-1)> y(i) <y(i+1) for min
+#
 
 function findExtrema(y)
 
