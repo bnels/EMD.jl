91028b780a7fa732a01ce8bb6084084db8e37da9
gabriel <gdmaher@stanford.edu>
2015-05-23 16:11:53 -0700

    Changed findExtrema to add boundary points

--- src/EMDUtil.jl
+++ src/EMDUtil.jl
@@ -3,12 +3,19 @@
 # y(i-1) < y(i) > y(i+1) for max and y(i-1)> y(i) <y(i+1) for min
 #
 
-function findExtrema(y, t)
+function findExtrema(y::Vector{Float64}, t::Vector{Float64})
 
-	max  = Any[]	#array for storing local maxima
-	min  = Any[]	#array for storing local minima
-	tMax = Any[]	#array for storing location of maxima
-	tMin = Any[]	#array for storing location of minima
+	max  = Float64[]	#array for storing local maxima
+	min  = Float64[]	#array for storing local minima
+	tMax = Float64[]	#array for storing location of maxima
+	tMin = Float64[]	#array for storing location of minima
+	n = length(y)
+
+	#add initial points to all arrays
+	push!(max, y[1])
+	push!(min, y[1])
+	push!(tMax, t[1])
+	push!(tMin, t[1])
 
 	for i = 2:length(y)-1
 		if(y[i-1] < y[i] && y[i] > y[i+1])
@@ -21,6 +28,12 @@ function findExtrema(y, t)
 			push!(tMin, t[i])
 		end
 	end
+
+	#add final points to all arrays
+	push!(max, y[n])
+	push!(min, y[n])
+	push!(tMax, t[n])
+	push!(tMin, t[n])
 
 	return max, min, tMax, tMin
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -5,11 +5,22 @@ include("../src/EMDUtil.jl")
 
 println("EMDUtil test started")
 #test case for findExtrema function
-y = [1 3 1 2]
-t = [1 2 3 4]
+y = Float64[1,3,1,2]
+t = Float64[1,2,3,4]
 max, min, tmax, tmin = findExtrema(y, t)
 
-@test max[1] == 3
-@test tmax[1] == 2
+@test max[1] == 1
+@test max[2] == 3
+@test max[3] == 2
+
+@test tmax[1] == 1
+@test tmax[2] == 2
+@test tmax[3] == 4
+
 @test min[1] == 1
-@test tmin[1] == 3
+@test min[2] == 1
+@test min[3] == 2
+
+@test tmin[1] == 1
+@test tmin[2] == 3
+@test tmin[3] == 4
