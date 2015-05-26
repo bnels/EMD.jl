388ded3624ce38890999010e0b371f2df238d0e4
Victor Minden <victorminden@gmail.com>
2015-05-25 17:12:17 -0700

    CHECK OUT EXAMPLE IT WORKS

--- src/EMD.jl
+++ src/EMD.jl
@@ -9,40 +9,67 @@ export IF
 
 
 #Function to calculate intrinsic mode functions
-function IMF(y, t, tol=0.01, order=4, N=5)
+#N is the maximum number of modes to find
+function IMF(y, t, toldev=0.01, tolzero = 0.01, order=4, N=5)
 
 	n = length(y)
 	f = zeros(n,N)
 	tempy = copy(y)
 	eps = 0.00001
 
+	n_modes = 0;
+
 	for i = 1:N
 	    avg = zeros(n,1)+1
-	    sd = 1.0
+	    sd = 2*toldev
 
-	    while(mean(abs(avg))>tol && sd > tol)
+	    while(mean(abs(avg))>tolzero && sd > toldev)
 
-	        max, min, tmax, tmin = findExtrema(tempy, t)
+	    	# Interpolate a spline through the maxima and minima
+	        max_ar, min_ar, tmax, tmin = findExtrema(tempy, t)
 
-	        S1 = Spline(max, tmax, order)
-	        S2 = Spline(min, tmin, order)
-	        avS = (S1+S2)/2
-	        avg = avS(t)
+	        # # Don't use too high an order to interpolate, restrict it if there are not many extrema
+	        # p_max = min(order, length(max_ar)-2)
+	        # p_min = min(order, length(min_ar)-2)
+
+	        # # Make even order
+	        # p_max = Int(2*floor(p_max/2))
+	        # p_min = Int(2*floor(p_min/2))
+
+	        # # At least linear
+	        # p_max = max(p_max,2)
+	        # p_min = max(p_min,2)
+	        p_max = (length(max_ar)-2 >= order)? order : 2
+	        p_min = (length(min_ar)-2 >= order)? order : 2
+
+	        S1 = Spline(max_ar, tmax, p_max)
+	        S2 = Spline(min_ar, tmin, p_min)
+
+	        # Find mean of envelope
+	        avg = (S1(t) + S2(t)) / 2
 	        tempy = tempy-avg
 
-	        sd = mean( (avg.^2)./((y-f[:,i]).^2 + eps) ) 
+	        sd = mean( (avg.^2)./((y-f[:,i]).^2 + eps) )
 
 	        f[:,i] = f[:,i] + avg
 
-	        println(sd)
+	        #println(sd)
 	    end
 
 	    tempy = copy(f[:,i])
+
+	    # Check to see if it's worth continuing or if the remainder is monotone
+	    c = mean(abs(tempy))
+	    d = diff(tempy)
+	    n_modes = n_modes + 1
+	    if all(d+c*tolzero .> 0) || all(d-c*tolzero .< 0)
+	    	break
+	    end
 	end
 
 	C = zeros(n,N)
 	C[:,1] = y - f[:,1]
-	for i = 2:N
+	for i = 2:n_modes
 		C[:,i] = f[:,i-1]-f[:,i]
 	end
 
