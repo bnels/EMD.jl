6b1738650e7e00235d5c1087e3255cfdfa7fa1ec
gabriel <gmaher2@hotmail.com>
2015-05-26 17:15:35 -0700

    Made function for instantaneous frequencies, see notebook

--- src/EMD.jl
+++ src/EMD.jl
@@ -84,8 +84,30 @@ function IMF(y, t, toldev=0.01, tolzero = 0.01, order=4, N=5,window=0)
 end
 
 #function to calculate instantaneous frequencies
-function IF(C)
+function IF(C, t, x=0, order=4, window=0)
 
+	n,N = size(C)
+	n = length(t)
+
+	if window == 0
+		window = ones(lengh(t))
+	end
+
+	if x == 0
+		x = t+1e-6
+	end
+
+	nx = length(x)
+	Phi = zeros(nx,N)
+
+	for i =1:N
+		H1 = Spline(C[:,i], t, order)
+
+		Phi[:,i] = 1/(2pi)*(H1(x).*H1(x,1,true) - H1(x, 1).*H1(x,0,true))./(H1(x).^2 + H1(x,0,true).^2)
+		Phi[:,i] = Phi[:,i].*window
+	end
+
+	return Phi
 
 end
 
