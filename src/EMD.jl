3206ee48e9872e15917ff98f6f1f5b616dc246a6
gabriel <gmaher2@hotmail.com>
2015-06-02 17:01:30 -0700

    fixed bug with windowing

--- src/EMD.jl
+++ src/EMD.jl
@@ -89,12 +89,12 @@ function IF(C, t, x=0, order=4, window=0)
 	n,N = size(C)
 	n = length(t)
 
-	if window == 0
-		window = ones(length(t))
-	end
-
 	if x == 0
 		x = t+1e-6
+	end
+
+	if window == 0
+		window = ones(length(x))
 	end
 
 	nx = length(x)
